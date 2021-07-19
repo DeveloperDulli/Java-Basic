6bfd466525188f7a026d4fd4f224fc38532d453b
DeveloperDulli <dulli1616@kakao.com>
2021-07-19 17:47:28 +0900

    Java3

---
+++ java3/StringApp.java
@@ -0,0 +1,22 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World"); // String(문자열)
+		System.out.println('H'); // Character(문자)
+		System.out.println("H");
+// 문자열과 문자의 차이 (큰따옴표, 작은따옴표)		
+		
+		System.out.println("Hello "
+				+ "World"); 
+		
+		System.out.println("Hello \nWorld");
+// (\n: new line) 줄바꿈 예시
+		
+		System.out.println("Hello \"World\"");
+// escape		
+
+	}
+
+}
